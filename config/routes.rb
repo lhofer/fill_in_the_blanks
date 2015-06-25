c7ebdbfac02b919791729f6979e8717e1867deb2
Leila Hofer <lkhofer@gmail.com>
2015-06-25 21:20:39 +0200

    finished fill in the blanks required section

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,20 @@ Rails.application.routes.draw do
 
   # Below is your index route, finish filling in the the remaining routes
   # What method can we use to create all of our RESTful routes at once?
-  get '/posts', to: 'posts#index', as: 'posts'
+  get '/posts(.:format)', to: 'posts#index', as: 'posts'
+  post '/posts(.:format)', to: 'posts#create' 
+  get '/posts/new(.:format)', to: 'posts#new', as: 'new_post' 
+  get '/posts/:id/edit(.:format)', to: 'posts#edit', as: 'edit_post'
+  get '/posts/:id(.:format)', to: 'posts#show', as: 'post'
+  patch '/posts/:id(.:format)', to: 'posts#update'
+  put '/posts/:id(.:format)', to: 'posts#update'
+  delete '/posts/:id(.:format)', to: 'posts#destroy'
+
 
   # This root route is pointing to the wrong controller, fix it
-  root 'welcome#index'
+  root 'posts#index'
 
 end
+
+
+              
